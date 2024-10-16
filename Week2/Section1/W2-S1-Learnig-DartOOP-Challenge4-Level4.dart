7f5579c796cf92aad8470007253da76ff7d9791a
HY Menghour <hymenghour2@gmail.com>
2024-10-16 14:56:44 +0700

    fix

--- Week2/Section1/W2-S1-Learnig-DartOOP-Challenge4-Level4.dart
+++ Week2/Section1/W2-S1-Learnig-DartOOP-Challenge4-Level4.dart
@@ -5,28 +5,33 @@ class Distance {
 
   Distance.meters(double meters) : _cms = meters * 100;
 
-
   Distance.kms(double kms) : _cms = kms * 100000;
-
 
   double get cms => _cms;
   double get meters => _cms / 100;
   double get kms => _cms / 100000;
 
-
   Distance operator +(Distance other) {
     return Distance.cms(this._cms + other._cms);
   }
 
-
   @override
-  String toString() => 'Distance: ${_cms / 100000} km';
+  String toString() {
+    if (_cms >= 100000) {
+      return 'Distance: ${kms} km';
+    } else if (_cms >= 100) {
+      return 'Distance: ${meters} meters';
+    } else {
+      return 'Distance: ${cms} cms';
+    }
+  }
 }
 
 void main() {
-
   Distance d1 = Distance.kms(3.4);
   Distance d2 = Distance.meters(1000);
 
-  print('Total Distance in km: ${(d1 + d2).kms} km');
+  Distance totalDistance = d1 + d2;
+
+  print('Total Distance: ${totalDistance.toString()}');
 }
